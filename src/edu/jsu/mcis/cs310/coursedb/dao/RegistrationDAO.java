95b590146391b5cd093233684eb1fc1f6bf8b85b
Justin Toliver <jtoliver@stu.jsu.edu>
2023-09-28 14:58:00 -0500

    Committing final touches to files

--- src/edu/jsu/mcis/cs310/coursedb/dao/RegistrationDAO.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/RegistrationDAO.java
@@ -1,5 +1,6 @@
 package edu.jsu.mcis.cs310.coursedb.dao;
 
+import com.github.cliftonlabs.json_simple.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,8 +9,8 @@ import java.sql.Statement;
 
 public class RegistrationDAO {
     
-    // INSERT YOUR CODE HERE
-    
+    private final String QUERY_CREATE = "INSERT INTO registration (studentid, termid, crn) VALUES (?, ?, ?)";
+    private final String QUERY_LIST = "SELECT * FROM registration WHERE studentid = ? AND termid = ?";
     private final DAOFactory daoFactory;
     
     RegistrationDAO(DAOFactory daoFactory) {
@@ -18,6 +19,7 @@ public class RegistrationDAO {
     
     public boolean create(int studentid, int termid, int crn) {
         
+        // Query Vars
         boolean result = false;
         
         PreparedStatement ps = null;
@@ -29,7 +31,16 @@ public class RegistrationDAO {
             
             if (conn.isValid(0)) {
                 
-                // INSERT YOUR CODE HERE
+                // Set Query and prepare statement
+                ps = conn.prepareStatement(QUERY_CREATE, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+                
+                // Execute
+                
+                result = ps.executeUpdate() != 0;
+                
                 
             }
             
@@ -50,7 +61,10 @@ public class RegistrationDAO {
 
     public boolean delete(int studentid, int termid, int crn) {
         
+        // Quey Vars
         boolean result = false;
+        int keys; 
+        String query;
         
         PreparedStatement ps = null;
         
@@ -60,7 +74,15 @@ public class RegistrationDAO {
             
             if (conn.isValid(0)) {
                 
-                // INSERT YOUR CODE HERE
+                // Set Query and prepare statement
+                query = "DELETE FROM registration WHERE studentid = ? AND termid = ? AND crn = ?";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+                
+                // Execute
+                result = ps.executeUpdate() != 0;
                 
             }
             
@@ -80,7 +102,10 @@ public class RegistrationDAO {
     
     public boolean delete(int studentid, int termid) {
         
+        // Query Vars
         boolean result = false;
+        int keys;
+        String query;
         
         PreparedStatement ps = null;
         
@@ -90,7 +115,14 @@ public class RegistrationDAO {
             
             if (conn.isValid(0)) {
                 
-                // INSERT YOUR CODE HERE
+                // Set Query and prepare statement
+                query = "DELETE FROM registration WHERE studentid = ? AND termid = ?";
+                ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                
+                // Execute
+                result = ps.executeUpdate() != 0;
                 
             }
             
@@ -111,6 +143,9 @@ public class RegistrationDAO {
     public String list(int studentid, int termid) {
         
         String result = "[]";
+        boolean hasResults;
+        
+        JsonArray jsonArray = new JsonArray();
         
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -122,7 +157,32 @@ public class RegistrationDAO {
             
             if (conn.isValid(0)) {
                 
-                // INSERT YOUR CODE HERE
+                // Use prepared statement to stage the "SELECT * FROM " argument
+                ps = conn.prepareStatement(QUERY_LIST);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                
+                // Execute 
+                hasResults = ps.execute();
+                
+                // Check if results
+                if (hasResults) {
+                    
+                    /* Get ResultSet Metadata */
+                    // Serialize and Submit
+                    rs = ps.getResultSet();
+                    result = DAOUtility.getResultSetAsJson(rs);
+                    
+                }
+                
+                /* If no data available, print an error */
+
+                else {
+
+                    System.err.println("ERROR: No data returned!");
+
+                }
+                
                 
             }
             
--- src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import com.github.cliftonlabs.json_simple.*;
 
 public class SectionDAO {
     
-    // INSERT YOUR CODE HERE
-    
+    // INESERT YOUR CODE HERE
     private final DAOFactory daoFactory;
     
     SectionDAO(DAOFactory daoFactory) {
@@ -18,7 +18,11 @@ public class SectionDAO {
     public String find(int termid, String subjectid, String num) {
         
         String result = "[]";
+        //Variables for using querys
+        String query;
+        boolean hasResults;
         
+        // Prepared Vars
         PreparedStatement ps = null;
         ResultSet rs = null;
         ResultSetMetaData rsmd = null;
@@ -29,8 +33,39 @@ public class SectionDAO {
             
             if (conn.isValid(0)) {
                 
-                // INSERT YOUR CODE HERE
+                // Use prepared statement to stage the "SELECT * FROM " argument
+                query = "SELECT * FROM section WHERE termid = ? AND subjectid = ? AND num = ?";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, termid);
+                ps.setString(2, subjectid);
+                ps.setString(3, num);
                 
+                // Execute 
+                hasResults = ps.execute();
+                
+                /* Get Results */
+                
+                System.out.println("Getting Results ...");
+                
+                // Check if results
+                if (hasResults) {
+                    
+                    /* Get ResultSet Metadata */
+                    
+                    rs = ps.getResultSet();
+                    
+                    /* Serialize and Submit */
+                    
+                    result = DAOUtility.getResultSetAsJson(rs);
+                }
+                
+                /* If no data available, print an error */
+
+                else {
+
+                    System.err.println("ERROR: No data returned!");
+
+                }
             }
             
         }
