87d391fbe1ef3c8402b86d78d7e18c35fd70abca
Justin Toliver <jtoliver@stu.jsu.edu>
2023-09-28 14:54:29 -0500

    Fixed DAOUtility, RegistrationDAO, and SectionDAO files

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -2,7 +2,6 @@ package edu.jsu.mcis.cs310.coursedb.dao;
 
 import java.sql.*;
 import com.github.cliftonlabs.json_simple.*;
-import java.util.ArrayList;
 
 public class DAOUtility {
     
@@ -11,12 +10,27 @@ public class DAOUtility {
     public static String getResultSetAsJson(ResultSet rs) {
         
         JsonArray records = new JsonArray();
+        ResultSetMetaData rsmd = null;
+        int columnCount;
         
         try {
         
             if (rs != null) {
 
-                // INSERT YOUR CODE HERE
+                rsmd = rs.getMetaData();
+                columnCount = rsmd.getColumnCount();
+                    
+                /* Loop through result set and append to json array as json objects */
+                while (rs.next()) {
+                        
+                    JsonObject jsonObject = new JsonObject();
+                        
+                    for (int i = 1; i <= columnCount; ++i) {
+                        jsonObject.put(rsmd.getColumnLabel(i), rs.getString(i));
+                    }
+                        
+                    records.add(jsonObject);
+                }
 
             }
             
